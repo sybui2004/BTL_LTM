1a13f394cd9a46feb59dce6255857741f746698f
sybui2004 <symerline2004@gmail.com>
2025-09-06 16:34:56 +0700

    chore: project set up springboot + libgdx

---
+++ BE/src/main/java/com/ltm/memorygame/MemoryCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.ltm.memorygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryCardGameApplication.class, args);
+	}
+
+}
